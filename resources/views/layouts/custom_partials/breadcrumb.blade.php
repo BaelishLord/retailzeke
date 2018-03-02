a4b8bc1c7b4233f0bc76e8185f89aeba00a6f9d0
BaelishLord <gupta.anmol635@gmail.com>
2018-03-02 15:00:39 -0800

    message

--- resources/views/layouts/custom_partials/breadcrumb.blade.php
+++ resources/views/layouts/custom_partials/breadcrumb.blade.php
@@ -1,15 +1,13 @@
-@if(Request::segment(3) != "create")
 <ol class="breadcrumb">
     <li><a href="/{{Request::segment(1)}}"><i class="fa fa-users"></i> {{ucwords(str_replace('-', ' ', Request::segment(1)))}} </a></li>
         <li><a href="/{{Request::segment(1)}}/{{Request::segment(2)}}/"><i class="fa fa-user"></i> {{ucwords(str_replace('-', ' ', Request::segment(2)))}} </a></li>
     @if (Request::segment(4))
         <li><a href="javascript:;"><i class="fa fa-pencil"></i> {{ucwords(str_replace('-', ' ', Request::segment(4)))}} </a></li>
     @else
-        @if(Request::segment(3) || Request::segment(3) == "create")
+        @if(Request::segment(3) == "create")
             <li><a href="javascript:;"><i class="fa fa-plus"></i> {{ucwords(str_replace('-', ' ', Request::segment(3)))}} </a></li>
         @elseif (Route::currentRouteName() == Request::segment(2).".show")
             <li><a href="javascript:;"><i class="fa fa-eye"></i> {{ucwords(str_replace('-', ' ','show'))}} </a></li>
         @endif
     @endif
 </ol>
-@endif
